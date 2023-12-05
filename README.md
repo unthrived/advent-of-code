70616e2c1d10ce3d710009a5e3c46c97ebaa241f
Junjie Ji <111136649+unthrived@users.noreply.github.com>
2023-12-05 12:38:07 +0100

    aoc

--- README.md
+++ README.md
@@ -8,8 +8,8 @@
 | [Day 1](https://github.com/unthrived/advent-of-code-2023/blob/main/src/day01.py) | :star: :star: |
 | [Day 2](https://github.com/unthrived/advent-of-code-2023/blob/main/src/day02.py) | :star: :star: |
 | [Day 3](https://github.com/unthrived/advent-of-code-2023/blob/main/src/day03.py) | :star: :star: |
-| [Day 4](https://github.com/unthrived/advent-of-code-2023/blob/main/src/day04.py) | :soon: :soon: |
-| [Day 5](https://github.com/unthrived/advent-of-code-2023/blob/main/src/day05.py) | :soon: :soon: |
+| [Day 4](https://github.com/unthrived/advent-of-code-2023/blob/main/src/day04.py) | :star: :star: |
+| [Day 5](https://github.com/unthrived/advent-of-code-2023/blob/main/src/day05.py) | :star: :soon: |
 | [Day 6](https://github.com/unthrived/advent-of-code-2023/blob/main/src/day06.py) | :soon: :soon: |
 | [Day 7](https://github.com/unthrived/advent-of-code-2023/blob/main/src/day07.py) | :soon: :soon: |
 | [Day 8](https://github.com/unthrived/advent-of-code-2023/blob/main/src/day08.py) | :soon: :soon: |
---
+++ src/day05.py
@@ -0,0 +1,55 @@
+from utils import read_input
+data = read_input(day=5)
+
+n = len(data)
+nums = [str(i) for i in range(0, 10)]+['']
+blanks = []
+
+initial_seeds = data[0].split(': ')[1].split(' ')
+# initial_seeds = int(initial_seeds)
+for i in range(len(initial_seeds)):
+    initial_seeds[i] = int(initial_seeds[i])
+next = [0]*len(initial_seeds)
+print(initial_seeds)
+
+# seed = []
+# soil = []
+# fert = []
+# water = []
+# light = []
+# temp = []
+# humid = []
+# loc = []
+
+print(nums)
+for i in range(n):
+    if data[i]=='': blanks.append(i)
+blanks.append(n)
+highest = 0
+for i in range(1,n):
+    if i not in blanks:
+        if i-1 not in blanks:
+            data[i] = data[i].split(' ')
+            for j in range(len(data[i])):
+                data[i][j] = int(data[i][j])
+                if data[i][j] > highest:
+                    highest = data[i][j]
+
+for k in range(
+    #0, 2
+    len(initial_seeds)
+    ):
+    for j in range(len(blanks)-1):
+        for i in range(blanks[j]+3, blanks[j+1]):
+            # print(data[i-3])
+            substraction = initial_seeds[k] - data[i][1]
+            if 0 < substraction < data[i][2]: 
+                next[k] = data[i][0] + substraction
+                print(initial_seeds[k], data[i], next[k])
+        initial_seeds[k] = next[k]
+
+lowest = highest
+for k in range(len(initial_seeds)):
+    if initial_seeds[k] < lowest: lowest = initial_seeds[k]
+
+print(lowest)
